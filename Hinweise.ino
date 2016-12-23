0e8a54d5bca33e4cf3ee2ffe92b92cc3a40a93a6
HunziHL <cwd.hl@gmx.de>
2016-12-23 15:23:53 +0100

    Initial Upload

---
+++ Hinweise.ino
@@ -0,0 +1,51 @@
+/*
+PICCOLO is a tiny Arduino-based audio visualizer.
+
+Hardware requirements:
+ - Most Arduino or Arduino-compatible boards (ATmega 328P or better).
+ - Adafruit Bicolor LED Matrix with I2C Backpack (ID: 902)
+ - Adafruit Electret Microphone Amplifier (ID: 1063)
+ - Optional: battery for portable use (else power through USB)
+Software requirements:
+ - elm-chan's ffft library for Arduino
+
+Connections:
+ - 3.3V to mic amp+ and Arduino AREF pin <-- important!
+ - GND to mic amp-
+ - Analog pin 0 to mic amp output
+ - +5V, GND, SDA (or analog 4) and SCL (analog 5) to I2C Matrix backpack
+
+Written by Adafruit Industries.  Distributed under the BSD license --
+see license.txt for more information.  This paragraph must be included
+in any redistribution.
+
+ffft library is provided under its own terms -- see ffft.S for specifics.
+*/
+
+// IMPORTANT: FFT_N should be #defined as 128 in ffft.h.
+
+// MATRIX DECLARATION:
+// Parameter 1 = width of NeoPixel matrix
+// Parameter 2 = height of matrix
+// Parameter 3 = pin number (most are valid)
+// Parameter 4 = matrix layout flags, add together as needed:
+//   NEO_MATRIX_TOP, NEO_MATRIX_BOTTOM, NEO_MATRIX_LEFT, NEO_MATRIX_RIGHT:
+//     Position of the FIRST LED in the matrix; pick two, e.g.
+//     NEO_MATRIX_TOP + NEO_MATRIX_LEFT for the top-left corner.
+//   NEO_MATRIX_ROWS, NEO_MATRIX_COLUMNS: LEDs are arranged in horizontal
+//     rows or in vertical columns, respectively; pick one or the other.
+//   NEO_MATRIX_PROGRESSIVE, NEO_MATRIX_ZIGZAG: all rows/columns proceed
+//     in the same order, or alternate lines reverse direction; pick one.
+//   See example below for these values in action.
+// Parameter 5 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+
+
+// Example for NeoPixel Shield.  In this application we'd like to use it
+// as a 5x8 tall matrix, with the USB port positioned at the top of the
+// Arduino.  When held that way, the first pixel is at the top right, and
+// lines are arranged in columns, progressive order.  The shield uses
+// 800 KHz (v2) pixels that expect GRB color data.
